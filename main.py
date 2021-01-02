090483424175f3bcf7f105d7505fdcf399e392c7
francesco-ftk <francesco.fantechi3@stud.unifi.it>
2021-01-02 15:06:36 +0100

    Skin_Detection

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ from test_and_training_set import save_frame_from_video, create_training_and_tes
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix
-from table_plot_functions import printTable, printConfusionMatrix
-from plot_functions import graphic_plot, histogram_plot
+from table_plot_functions import printTable, printConfusionMatrix, histogram_plot
 
 precision=[]
 recall=[]
 accuracy=[]
 classifier=[]
-f_score_histogram=[]
 f_score=[]
 
 # Salvataggio frame da video
@@ -20,11 +18,11 @@ Xtraining, Xtest, ytraining, ytrue= create_training_and_test_set(totalFrame)
 print("#samples: ", len(Xtraining), "#features: ", 3)
 
 # Classificatore Gaussian Naive Bayes
-print("Gaussian_Naive_Bayes_Classifier... ")
+print("Gaussian_Naive_Bayes_Classifier")
 clf=GaussianNB()
 clf.fit(Xtraining,ytraining)
 print("Training finished")
-f_score_histogram.append(round(clf.score(Xtest,ytrue),2))
+f_score.append(round(clf.score(Xtest,ytrue),2))
 prediction=clf.predict(Xtest)
 # Gaussian_Naive_Bayes Contingency Matrix
 tn, fp, fn, tp = confusion_matrix(ytrue, prediction).ravel()
@@ -41,34 +39,28 @@ accuracy.append(a)
 classifier.append("Gaussian_Naive_Bayes")
 
 # Classificatore Random Forest
-n_estimators=[1,4,7,10,11]
-for i in range(len(n_estimators)):
-    print("Random_Forest_Classifier, #estimators: %d" % n_estimators[i])
-    clf=RandomForestClassifier(n_estimators=n_estimators[i], criterion='gini', max_features='auto', bootstrap=True, max_samples=None)
-    clf.fit(Xtraining,ytraining)
-    print("Training completed")
-    fscore=round(clf.score(Xtest,ytrue),2)
-    f_score.append(fscore)
-    if n_estimators[i]==10:
-        f_score_histogram.append(fscore)
-        prediction=clf.predict(Xtest)
-        #Random_Forest Contingency Matrix
-        tn, fp, fn, tp = confusion_matrix(ytrue, prediction).ravel()
-        printConfusionMatrix(tn, fp, fn, tp)
-        p=tp/(tp+fp)
-        r=tp/(tp+fn)
-        a=(tp+tn)/(tp+fp+fn+tn)
-        p=round(p,3)
-        r=round(r,3)
-        a=round(a,3)
-        precision.append(p)
-        recall.append(r)
-        accuracy.append(a)
-        classifier.append("Random_Forest")
+print("Random_Forest_Classifier")
+clf=RandomForestClassifier(n_estimators=10, criterion='gini', max_features='auto', bootstrap=True, max_samples=None)
+clf.fit(Xtraining,ytraining)
+print("Training completed")
+f_score.append(round(clf.score(Xtest,ytrue),2))
+prediction=clf.predict(Xtest)
+#Random_Forest Contingency Matrix
+tn, fp, fn, tp = confusion_matrix(ytrue, prediction).ravel()
+printConfusionMatrix(tn, fp, fn, tp)
+p=tp/(tp+fp)
+r=tp/(tp+fn)
+a=(tp+tn)/(tp+fp+fn+tn)
+p=round(p,3)
+r=round(r,3)
+a=round(a,3)
+precision.append(p)
+recall.append(r)
+accuracy.append(a)
+classifier.append("Random_Forest")
 
 # Stampa risultati
-histogram_plot(f_score_histogram)
-graphic_plot(n_estimators,f_score)
+histogram_plot(f_score)
 printTable(classifier,precision,recall,accuracy)
 
 
--- table_plot_functions.py
+++ table_plot_functions.py
@@ -1,4 +1,5 @@
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
 def printTable(classifier,precision,recall,accuracy):
     table= go.Figure(data=[go.Table(header=dict(values=['Classifier','Precision','Recall','Accuracy'],align=['left'],font=dict(size=13)),
@@ -9,6 +10,14 @@ def printConfusionMatrix(tn, fp, fn, tp):
     table= go.Figure(data=[go.Table(header=dict(values=['Prediction/True_Class','Skin','Background'],align=['center'],font=dict(size=13)),
     cells=dict(values=[['Skin','Background'],[tp,fn],[fp,tn]],align='center',font_size=13))])
     table.show()
+
+def histogram_plot(f_score):
+    y = [1,2]
+    plt.barh(y,f_score)
+    plt.yticks(y,["Gaussian_Naive_Bayes","Random_Forest"])
+    plt.xlabel('F-score')
+    plt.show()
+
 
 
 
